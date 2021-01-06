181ddefd85c93c153ccdfd4fe3e9ee1836471ab9
Luis Rujana <lmrujanass@gmail.com>
2021-01-05 22:45:17 -0500

    Created connection.js

---
+++ config/connection.js
@@ -0,0 +1,23 @@
+const mysql = require('mysql');
+
+const source = {
+    localhost: {
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: 'myPassword123',
+        database: 'burguer_db'
+    }
+};
+
+const connection = mysql.createConnection(source.localhost);
+
+connection.connect(function(err){
+    if(err){
+        console.log(`error connecting ${err.stack}`);
+        return;
+    };
+    console.log(`connected as id ${connection.threadId}`);
+});
+
+module.exports = connection;
